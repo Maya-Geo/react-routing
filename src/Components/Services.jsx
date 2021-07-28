54245136665aff3ccfdaa63922517028d878ecfc
Maya Hagan <miyaclessa@gmail.com>
2021-07-29 04:18:01 +1200

    add Services.jsx

---
+++ src/Components/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div style ={{textAlign: "center"}}>
+              <h1>Services Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illum ipsum excepturi fugit sapiente esse dicta eveniet ducimus.
+        </div>
+    );
+}
+
+export default Services;
