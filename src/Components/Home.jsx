22aa9c8805cfbb346a7b2e44b224dfd7ec0bb431
Maya Hagan <miyaclessa@gmail.com>
2021-07-29 04:16:20 +1200

    add Home.jsx

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style ={{textAlign: "center"}}>
+              <h1>Home Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illum ipsum excepturi fugit sapiente esse dicta eveniet ducimus. 
+        </div>
+    );
+}
+
+export default Home;
