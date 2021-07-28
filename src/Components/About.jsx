2dee441a5060239a75fdc648b09d5972c9673b03
Maya Hagan <miyaclessa@gmail.com>
2021-07-29 04:16:50 +1200

    add about.jsx

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div style ={{textAlign: "center"}}>
+              <h1>About Page</h1>
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illum ipsum excepturi fugit sapiente esse dicta eveniet ducimus.
+        </div>
+    );
+}
+
+export default About;
