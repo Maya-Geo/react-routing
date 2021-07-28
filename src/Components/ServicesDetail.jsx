005559b0678644a010bcfabddd755e19f5813ecc
Maya Hagan <miyaclessa@gmail.com>
2021-07-29 04:19:54 +1200

    add ServicesDetail.jsx

---
+++ src/Components/ServicesDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = (props) => {
+    return (
+        <div>
+              <h1>Services Detail Page[{props.match.params.id}]</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illum ipsum excepturi fugit sapiente esse dicta eveniet ducimus. 
+
+        </div>
+    );
+}
+
+export default Services;
